2b072cf2b9273fa148571e4d5c1bef991fa3545c
Nikki <nikki.ng.nguyen@gmail.com>
2019-03-25 15:18:39 -0700

    finished pthread

--- pthreadLab/CounterController.cpp
+++ pthreadLab/CounterController.cpp
@@ -12,6 +12,7 @@ int speed_status = 0;
 float speed = 0.125;
 void *inputReader( void *param );
 void *countDownThread( void *param );
+bool flag = true;
 
 int main( int argc, char ** argv){
 
@@ -28,7 +29,6 @@ int main( int argc, char ** argv){
 }
 void *inputReader( void *param ){
   cout << "!!! Reader Thread Running!\n";
-  bool flag = true;
 	value = 10;	// change value to make countDownThread unblock and begin counting
 
   cout << "Enter a command:\n+: increase speed count\n-: decrease speed count\ns: switch setting\nd: direction setting\na: quit\n";
@@ -37,24 +37,30 @@ void *inputReader( void *param ){
   cout << "current count speed: " << speed << endl;
   cout << "current counter: ENABLED" << endl;
   cout << "current direction: UP" << endl;
-  do{
+  while(input != 'a'){
     cin >> input;
     if(!((input=='+') ||(input =='-') || (input == 's') || (input == 'd') || (input == 'a'))){
       cin.clear();
       cin.ignore();
     }
-    if(input == '-' && speed != 1){
+    if(input == '-'){
       if(speed == 1){ //if in setting 0, stay at 0
         speed = 1;
       }
-      speed = speed * 2;
+      else {
+        speed = speed * 2;
+      }
+
       cout << "current count speed: " << speed << endl;
     }
-    else if(input == '+' && speed != 0.125){
+    else if(input == '+'){
       if(speed == 0.125){
         speed = 0.125;
       }
-      speed = speed/2;
+      else{
+        speed = speed/2;
+      }
+
       cout << "current count speed: " << speed << endl;
     }
     if(input == 's'){ //switch counter
@@ -80,53 +86,71 @@ void *inputReader( void *param ){
       cout << "inputReader thread exited\n";
     	pthread_exit( 0 );
     }
+  }
+  return 0;
 
-  }while(flag == true);
-		cin >> input;	// BLOCKING operation
-    cout << "inputReader thread exited\n";
-  	pthread_exit( 0 );
 }
 
 void *countDownThread( void *param ){
 	cout << "!!! Count Down Thread Running!\n";
 	struct timespec timing;
-	while( value == 0 );	// BLOCK while value equals 0
-	while( value > 0 ){
+  timing.tv_sec = 0;
+	while( flag != false){
 		// sleep( 1 );							// wait 1 second
     if(speed == .125){
-      timing.tv_sec = 0;
     	timing.tv_nsec = 125000000L;
+      nanosleep( &timing, NULL );
     }
     else if(speed == .25){
-      timing.tv_sec = 0;
     	timing.tv_nsec = 250000000L;
+      nanosleep( &timing, NULL );
     }
     else if(speed == .5){
-      timing.tv_sec = 0;
     	timing.tv_nsec = 500000000L;
+      nanosleep( &timing, NULL );
     }
     else if(speed == 1){
-      timing.tv_sec = 1;
-    	timing.tv_nsec = 000000000L;
+      sleep (1);
+      nanosleep( &timing, NULL );
     }
     if (!counter){
       cin.get();
-    }else{
-      if (!direction){
-        cout << --value << flush;
-      }else{
-        cout << ++value << flush;	// flush to make the value display
-      }
     }
+    else{
+      if(!direction){
+        cout << --value << flush;
+        while(value == 0){
+          value = 99;
+        }
+      }
+      else{
+        cout << ++value << flush;
+        while(value == 99){
+          value = 0;
+        }
+      }
+      /*while(input != 'a'){
+        if(!direction){
+
+          cout << --value << flush;
+        }else{
+
+        cout << ++value << flush;	// flush to make the value display
+        }
+
+
+    }*/
+  }
     if(input == 'a'){
       cout << "countDownThread thread exited\n";
     	pthread_exit( 0 );
     }
-    nanosleep( &timing, NULL );
-		// cout << "\r                       \r";	// overwrite previous number with spaces
+
+		cout << "\r                       \r";	// overwrite previous number with spaces
 		cout << "\b\b\b\b\b\b       \b\b\b\b\b\b";	// overwrite previous number with spaces
-		// cout << "\b\b\b\b\b\b";	// backspace over previously  displayed characters
+		cout << "\b\b\b\b\b\b";	// backspace over previously  displayed characters
 	}
-	cout << "countDownThread thread exited\n";
-	pthread_exit( 0 );
+	//cout << "countDownThread thread exited\n";
+	//pthread_exit( 0 );
+  return 0;
 }
