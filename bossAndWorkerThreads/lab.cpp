2bed385a9799dc72dbda352aacf40ee8e1ead757
Nikki <nikki.ng.nguyen@gmail.com>
2019-04-15 13:43:09 -0700

    labboss+worker update 1

--- bossAndWorkerThreads/lab.cpp
+++ bossAndWorkerThreads/lab.cpp
@@ -25,81 +25,7 @@ static void cleanupHandler( void *arg ){
 
 short signify = 0; //thread launch delay variable
 
-void *worker( void *param ){
-  WorkerInfo myInfo(*(WorkerInfo*)param);
-  cout << "Worker Thread " <<  myInfo.getWorkerID()  << " Running!\n" <<endl;
-  signify++;
-  pthread_cleanup_push(cleanupHandler, NULL);
-  while(1){
-      pthread_mutex_lock( &theMutex );
-      pthread_cond_wait(&tEnable[myInfo.getWorkerID()-1], &theMutex);
-      myInfo.showDataSet();
-      pthread_mutex_unlock( &theMutex );
-    }
-      pthread_cleanup_pop(cleanup_pop_arg);
-}
 
-void *boss( void *param ){
-  short runningWorkers = *(short*)param;
-  bool workerRunning[*(short*)param];
-  int input = 0;
-  int i = 0;
-
-  do{
-    workerRunning[i] = true;
-    i++;
-  }while(i < *(short*)param);
-
-  cout << "!!! boss Thread Running!\n" << "managing " << *(short*)param << " worker threads\n" << endl;
-  cout << "Enter a number of 1 to " << *(short *)param << " to make a thread print it's data set\n"
-  << "Enter the negated value of a worker thread to cancel that thread\n"
-  << "Enter 0 to make all threads output their data sets\n" << endl;
-
-  do{
-    cout << " > ";
-    cin >> input;
-    if(input == 0){
-      i = 0;
-      while(i < *(short*)param){
-        if(workerRunning[i]){
-          pthread_cond_signal( &tEnable[i] );
-        }
-        i++;
-      }
-      continue;
-    }
-    else if( (input > 0) && (input <= *(short*)param)){
-      if(workerRunning[input -1]){
-        pthread_cond_signal( &tEnable[input-1]);
-        continue;
-      }
-      else{
-        cout << "worker " << input << " already finished\n" << endl;
-        continue;
-    }
-    }
-    else if((input < 0) && ((-input) <= *(short*)param)){
-      input = -input;
-      if(workerRunning[input-1]){
-        cout << "Canceling worker " << input << endl;
-        cleanup_pop_arg = input;
-        pthread_cancel(tids[input-1]);
-        workerRunning[input-1] = false;
-        runningWorkers--;
-        continue;
-      }
-      else{
-        cout << "Worker " << input << " already canceled\n" <<endl;
-        continue;
-      }
-
-    }
-    cout << "Error: Invalid Entry\n" << endl;
-    continue;
-  }while(runningWorkers == true);
-  cout << "BOSS exits!\n";
-  pthread_exit(0);
-}
 //Worker Info Management Class
 
 //class for Worker Thread data
@@ -197,4 +123,79 @@ int main( int argc, char ** argv ){
   pthread_mutex_destroy( &theMutex );
   delete [ ] tids;  //deallocate heap memory
   delete [ ] tEnable; //deallocate heap memory
+}
+void *worker( void *param ){
+  WorkerInfo myInfo(*(WorkerInfo*)param);
+  cout << "Worker Thread " <<  myInfo.getWorkerID()  << " Running!\n";
+  signify++;
+  pthread_cleanup_push(cleanupHandler, NULL);
+  while(1){
+      pthread_mutex_lock( &theMutex );
+      pthread_cond_wait(&tEnable[myInfo.getWorkerID()-1], &theMutex);
+      myInfo.showDataSet();
+      pthread_mutex_unlock( &theMutex );
+    }
+      pthread_cleanup_pop(cleanup_pop_arg);
+}
+
+void *boss( void *param ){
+  short runningWorkers = *(short*)param;
+  bool workerRunning[*(short*)param];
+  int input = 0;
+  int i = 0;
+
+  do{
+    workerRunning[i] = true;
+    i++;
+  }while(i < *(short*)param);
+
+  cout << "!!! boss Thread Running!\n" << "managing " << *(short*)param << " worker threads\n" << endl;
+  cout << "Enter a number of 1 to " << *(short *)param << " to make a thread print it's data set\n"
+  << "Enter the negated value of a worker thread to cancel that thread\n"
+  << "Enter 0 to make all threads output their data sets\n" << endl;
+
+  do{
+    cout << " > ";
+    cin >> input;
+    if(input == 0){
+      i = 0;
+      while(i < *(short*)param){
+        if(workerRunning[i]){
+          pthread_cond_signal( &tEnable[i] );
+        }
+        i++;
+      }
+      continue;
+    }
+    else if( (input > 0) && (input <= *(short*)param)){
+      if(workerRunning[input -1]){
+        pthread_cond_signal( &tEnable[input-1]);
+        continue;
+      }
+      else{
+        cout << "worker " << input << " already finished\n" << endl;
+        continue;
+    }
+    }
+    else if((input < 0) && ((-input) <= *(short*)param)){
+      input = -input;
+      if(workerRunning[input-1]){
+        cout << "Canceling worker " << input << endl;
+        cleanup_pop_arg = input;
+        pthread_cancel(tids[input-1]);
+        workerRunning[input-1] = false;
+        runningWorkers--;
+        continue;
+      }
+      else{
+        cout << "Worker " << input << " already canceled\n" <<endl;
+        continue;
+      }
+
+    }
+    cout << "Error: Invalid Entry\n" << endl;
+    continue;
+  }while(runningWorkers == true);
+  cout << "BOSS exits!\n";
+  pthread_exit(0);
 }
